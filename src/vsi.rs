a88d8b4f4275bcf8d4c464a40a03c720037da009
kmaitreys <km.maitrey@gmail.com>
2025-07-10 00:04:49 +0530

    [feat] `vsi` should be single with two fns

---
+++ src/vsi.rs
@@ -0,0 +1,9 @@
+pub fn run() {
+    println!("Running vertical shear instability test");
+    todo!()
+}
+
+pub fn run_timed() {
+    println!("Running vertical shear instability test with output at a given time interval");
+    todo!()
+}
