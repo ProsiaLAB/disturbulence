09637a0d6e2b320016a9099b807baa050bae1ec6
kmaitreys <km.maitrey@gmail.com>
2025-07-10 19:21:37 +0530

    [impl] update config dispatch with correct dispatch

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 use std::env;
 
 use anyhow::Result;
-use fftw::array::AlignedVec;
-use fftw::plan::R2CPlan;
-use fftw::plan::R2CPlanMany;
-use fftw::types::Flag;
-use fftw::types::c64;
+// use fftw::array::AlignedVec;
+// use fftw::plan::R2CPlan;
+// use fftw::plan::R2CPlanMany;
+// use fftw::types::Flag;
+// use fftw::types::c64;
 
 use disturbulence::advection;
-use disturbulence::config::Config;
 use disturbulence::config::load_config;
 use disturbulence::euler;
 use disturbulence::hsbr;
+use disturbulence::modes::Config;
+use disturbulence::modes::RunType;
 use disturbulence::svf;
 use disturbulence::tcf;
 use disturbulence::user;
@@ -28,33 +29,33 @@ fn main() -> Result<()> {
 
     let config = load_config(file)?;
 
-    match config {
-        Config::UserApplication { .. } => user::run(),
-        Config::Euler1D { .. } => euler::run(),
-        Config::VerticalHydrostaticBalance { .. } => vhb::run(),
-        Config::SolidBodyRotation { .. } => hsbr::run(),
-        Config::SingleVortexFargoTest { .. } => svf::run(),
-        Config::VerticalShearInstability { .. } => vsi::run(),
-        Config::VortexPair { .. } => vp::run(),
-        Config::TaylorCouetteFlow { .. } => tcf::run(),
-        Config::AdvectionTest { .. } => advection::run(),
-        Config::TimedVsiOutput { .. } => vsi::run_timed(),
+    match config.run_type {
+        RunType::UserApplication => user::run(),
+        RunType::Euler1DTestZ => euler::run(config),
+        RunType::VerticalHydrostaticBalance => vhb::run(),
+        RunType::SolidBodyRotation => hsbr::run(),
+        RunType::SingleVortexFargoTest => svf::run(),
+        RunType::VerticalShearInstability => vsi::run(),
+        RunType::VortexPair => vp::run(),
+        RunType::TaylorCouetteFlow => tcf::run(),
+        RunType::AdvectionTest => advection::run(),
+        RunType::TimedVsiOutput => vsi::run_timed(),
     };
 
-    let nphi = 128;
-    let num_transforms = 16;
-    let total_len = nphi * num_transforms;
+    // let nphi = 128;
+    // let num_transforms = 16;
+    // let total_len = nphi * num_transforms;
 
-    let mut input: AlignedVec<f64> = AlignedVec::new(total_len);
-    let mut output: AlignedVec<c64> = AlignedVec::new((nphi / 2 + 1) * num_transforms);
+    // let mut input: AlignedVec<f64> = AlignedVec::new(total_len);
+    // let mut output: AlignedVec<c64> = AlignedVec::new((nphi / 2 + 1) * num_transforms);
 
-    let plan: fftw::plan::R2CPlan64 = R2CPlan::aligned(&[nphi], Flag::ESTIMATE).unwrap();
+    // let plan: fftw::plan::R2CPlan64 = R2CPlan::aligned(&[nphi], Flag::ESTIMATE).unwrap();
 
-    for i in 0..num_transforms {
-        let in_slice = &mut input[i * nphi..(i + 1) * nphi];
-        let out_slice = &mut output[i * (nphi / 2 + 1)..(i + 1) * (nphi / 2 + 1)];
-        plan.r2c(in_slice, out_slice).unwrap();
-    }
+    // for i in 0..num_transforms {
+    //     let in_slice = &mut input[i * nphi..(i + 1) * nphi];
+    //     let out_slice = &mut output[i * (nphi / 2 + 1)..(i + 1) * (nphi / 2 + 1)];
+    //     plan.r2c(in_slice, out_slice).unwrap();
+    // }
 
     Ok(())
 }
